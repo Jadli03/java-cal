e99b8b9ff24c29bd3e2f7f37e6778e541b970a06
jadli03 <surajjadli@yahoo.com>
2022-07-08 15:48:59 +0530

    pushing cal.java

---
+++ Cal.java
@@ -0,0 +1,10 @@
+public class Cal{
+    public static void main(String[] args) {
+        System.out.println("addition of 2 + 2 = ",add(2,2));
+        System.out.println("Subtraction of 2 + 2 = ",sub(2,2));
+    }
+
+    public static add(int a, int b){return a+b;}
+
+    public static sub(int a, int b) {return a-b;}
+}
