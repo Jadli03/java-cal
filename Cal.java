d1de835ef448de104038bab779ddb95e17340bb8
jadli03 <surajjadli@yahoo.com>
2022-07-08 16:04:57 +0530

    added +

--- Cal.java
+++ Cal.java
@@ -1,7 +1,7 @@
 public class Cal{
     public static void main(String[] args) {
-        System.out.println("addition of 2 + 2 = ",add(2,2));
-        System.out.println("Subtraction of 2 + 2 = ",sub(2,2));
+        System.out.println("addition of 2 + 2 = "+add(2,2));
+        System.out.println("Subtraction of 2 + 2 = "+sub(2,2));
     }
 
     public static int add(int a, int b){return a+b;}
